c28ddb1892fc9739301ee5daf804b8f493c51cb4
Jujian <jujian.zhang1998@outlook.com>
2022-06-07 12:15:12 +0100

    add more stuff about injective things

---
+++ src/enough_injectives/ab_has_enough_injectives.lean
@@ -0,0 +1,15 @@
+import enough_injectives.baer_criterion
+import category_theory.preadditive.injective
+
+section
+
+open category_theory
+
+universes u v
+variables {A : Type u} [add_comm_group A]
+
+
+
+-- instance : enough_injectives (Module.{u} ℤ) := sorry
+
+end
